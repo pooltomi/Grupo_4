79ddfdcf4a9189b7e731b494e99c228f31dba059
Pablo Mateu <Pablo Mateu@DESKTOP-SIVSDK3>
2021-05-06 21:25:02 -0300

    Solucion a la aparicion de datos una vez superada la cantidad de emplead

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
@@ -30,36 +31,52 @@ public class Main {
 
 		DecimalFormat format = new DecimalFormat("0.#");
 
-		System.out.println("Total Empleados Fabrica " + EMPLEADOS);
+		System.out.println("Total Empleados Fabrica " + format.format(EMPLEADOS));
 
 		System.out.println("Ingrese cantidad de empleados sector 1 : "); // recuerde que no puede ser mayor a "
 		// + format.format(EMPLEADOS) + " :");
 		totalDeEmpleadosSector1 = scan.nextInt();
 		scan.nextLine();
 		if (totalDeEmpleadosSector1 <= EMPLEADOS) {
+
 			restanEmpleados = (EMPLEADOS - totalDeEmpleadosSector1);
 
-			System.out.println("Ingrese cantidad de empleados sector 2  : "); // recuerde que no puede ser mayor a "
-			// + format.format(restanEmpleados) + " :");
-			totalDeEmpleadosSector2 = scan.nextInt();
-			if (totalDeEmpleadosSector2 <= restanEmpleados) {
-				restanEmpleados = (restanEmpleados - totalDeEmpleadosSector2);
+			if ((restanEmpleados != 0)) {
 
-				System.out.println("Ingrese cantidad de empleados sector 3 : "); // recuerde que no puede ser mayor a "
-				// + format.format(restanEmpleados) + " :");
-				totalDeEmpleadosSector3 = scan.nextInt();
-				if (totalDeEmpleadosSector3 <= restanEmpleados) {
-					restanEmpleados = (restanEmpleados - totalDeEmpleadosSector3);
-				} else {
-					System.out.println("Se supero la cantidad de empleados en el sector ");
+				if ((restanEmpleados != 0)) {
+					System.out.println("Ingrese cantidad de empleados sector 2  : "); // recuerde que no puede ser mayor
+																						// a "
+					// + format.format(restanEmpleados) + " :");
+					totalDeEmpleadosSector2 = scan.nextInt();
+
+					if ((totalDeEmpleadosSector2 <= restanEmpleados)) {
+
+						restanEmpleados = (restanEmpleados - totalDeEmpleadosSector2);
+
+						if ((restanEmpleados != 0)) {
+
+							System.out.println("Ingrese cantidad de empleados sector 3 : "); // recuerde que no puede
+																								// ser
+																								// mayor
+																								// a "
+							// + format.format(restanEmpleados) + " :");
+							totalDeEmpleadosSector3 = scan.nextInt();
+							if ((restanEmpleados != 0) && totalDeEmpleadosSector3 <= restanEmpleados) {
+
+								restanEmpleados = (restanEmpleados - totalDeEmpleadosSector3);
+							} else {
+								System.out.println("Se supero la cantidad de empleados en el sector ");
+							}
+
+						} else {
+							System.out.println("Se supero la cantidad de empleados en el sector ");
+						}
+
+					}
 				}
-
 			} else {
 				System.out.println("Se supero la cantidad de empleados en el sector ");
 			}
-
-		} else {
-			System.out.println("Se supero la cantidad de empleados en el sector ");
 		}
 
 		if (restanEmpleados <= 50) {
@@ -78,7 +95,7 @@ public class Main {
 							.println("Cantidad de empleados en el sector : " + format.format(totalDeEmpleadosSector1));
 
 					if (totalDeEmpleadosSector1 <= EMPLEADOS) {
-						for (int j = 0; j <= totalDeEmpleadosSector1; j++) {
+						for (int j = 1; j <= totalDeEmpleadosSector1; j++) {
 
 							temperatura = ((Math.random() * (40 - 35)) + 35);
 
@@ -102,7 +119,7 @@ public class Main {
 							.println("Cantidad de empleados en el sector : " + format.format(totalDeEmpleadosSector2));
 
 					if (totalDeEmpleadosSector2 <= EMPLEADOS) {
-						for (int j = 0; j <= totalDeEmpleadosSector2; j++) {
+						for (int j = 1; j <= totalDeEmpleadosSector2; j++) {
 
 							temperatura = ((Math.random() * (40 - 35)) + 35);
 
@@ -126,7 +143,7 @@ public class Main {
 							.println("Cantidad de empleados en el sector : " + format.format(totalDeEmpleadosSector3));
 
 					if (totalDeEmpleadosSector3 <= EMPLEADOS) {
-						for (int j = 0; j <= totalDeEmpleadosSector3; j++) {
+						for (int j = 1; j <= totalDeEmpleadosSector3; j++) {
 
 							temperatura = ((Math.random() * (40 - 35)) + 35);
 
@@ -147,9 +164,15 @@ public class Main {
 			}
 
 			casosGenerales = (cantidadDeCasos1 + cantidadDeCasos2 + cantidadDeCasos3);
-			porcentajeDeCasos1 = (double) ((cantidadDeCasos1 / totalDeEmpleadosSector1) * 100);
-			porcentajeDeCasos2 = (double) ((cantidadDeCasos2 / totalDeEmpleadosSector2) * 100);
-			porcentajeDeCasos3 = (double) ((cantidadDeCasos3 / totalDeEmpleadosSector3) * 100);
+			if (totalDeEmpleadosSector1 > 0) {
+				porcentajeDeCasos1 = (double) ((cantidadDeCasos1 / totalDeEmpleadosSector1) * 100);
+			}
+			if (totalDeEmpleadosSector2 > 0) {
+				porcentajeDeCasos2 = (double) ((cantidadDeCasos2 / totalDeEmpleadosSector2) * 100);
+			}
+			if (totalDeEmpleadosSector3 > 0) {
+				porcentajeDeCasos3 = (double) ((cantidadDeCasos3 / totalDeEmpleadosSector3) * 100);
+			}
 
 			System.out.println("\nLa cantidad de casos generales de COVID son: " + format.format(casosGenerales));
 			System.out.println("\nEl sector 1 tiene: " + format.format(porcentajeDeCasos1)
